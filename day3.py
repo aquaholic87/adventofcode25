2a4d23c89a9472c684aef1abc959aa8f68a3620e
SMAKWANA01 <2020SMakwana01@beauchampcity.org.uk>
2025-12-09 12:20:03 +0000

    finished day3

--- day3.py
+++ day3.py
@@ -1,11 +1,12 @@
 def main():
     banks = read_file() 
-    banks = ["987654321111111", "811111111111119", "234234234234278", "818181911112111"]
+    # banks = ["987654321111111", "811111111111119", "234234234234278", "818181911112111"]
     total = 0
     for bank in banks:
         result = highest_joltage(bank)
         total += result
         print(result)
+    print(total)
 
 def read_file():
     with open("day3.txt", "r") as file:
@@ -13,17 +14,29 @@ def read_file():
     return content
 
 def highest_joltage(bank):
-    bank = bank.split()
-    max1 = bank[0]
-    max2 = bank[1]
-    for i, num in enumerate(bank):
-        if i > len(bank):
-            break
-        if int(num) > max1:
-            max1 = num
-        elif int(num) > max2:
-            max2 = num
-    return int(max1) * int(max2)
+    bank = [int(num) for num in bank]
+    nums = []
+    for i in range(12):
+        nums.append(max(bank[:4]))
+        print(nums)
+        bank.remove(nums[i])
+    print(nums)
+    return int("".join([str(num) for num in nums]))
+
+
+# def highest_joltage(bank):#FOR PART 1
+#     bank = [int(number) for number in bank]
+#     print(bank)
+#     max1 = 0
+#     max2 = 0
+#     for i, num in enumerate(bank):
+#         if num > max1 and i < len(bank) -1:
+#             max1 = num
+#             max2 = bank[i+1]
+#         elif num > max2:
+#             max2 = num
+#     return int(str(max1) + str(max2))
 
 if __name__ == "__main__":
     main()
+
